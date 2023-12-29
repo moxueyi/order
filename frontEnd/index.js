8625d280a2102c553a28954e31c4b142efb94fba
moxueyi <moxueyi2333@gmail.com>
2023-12-29 20:02:07 +0800

    feat: 提交前端测试文件

--- frontEnd/index.js
+++ frontEnd/index.js
@@ -1,14 +1,34 @@
 const socket = new WebSocket("ws://127.0.0.1:10810/add?table=1");
 
 socket.onopen = function () {
-    // 发送一个初始化消息
-    socket.send("Hello, server!");
 }
 
 socket.onmessage = function (event) {
-    console.log(event.data);
+    let data = JSON.parse(event.data);
+    if (data["menu"]) {
+        console.log(data);
+    } else if (data["table"]) {
+        let tables = data["table"];
+        console.log(tables);
+    }
 }
 
-setTimeout(()=>{
-    socket.send("table 1 add "+ "凉菜");    
-}, 5000)
+setTimeout(() => {
+    socket.send("table 1 add " + "清蒸鲈鱼");
+}, 5000);
+
+setTimeout(() => {
+    socket.send("table 1 add " + "清蒸鲈鱼");
+}, 8000);
+
+setTimeout(() => {
+    socket.send("table 1 add " + "雪里红炒豆腐");
+}, 10000);
+
+setTimeout(() => {
+    socket.send("table " + `${1} ` + "sub " + "雪里红炒豆腐");
+}, 12000);
+setTimeout(() => {
+    socket.send("table 1 sub " + "雪里红炒豆腐");
+}, 13000);
+
